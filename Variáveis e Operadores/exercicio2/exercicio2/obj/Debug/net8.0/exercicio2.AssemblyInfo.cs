555a5d51200fbaacb8d5472274d396e800103f67
Rebeka <rebeka.silva.emailprofissional@gmail.com>
2025-03-16 23:28:01 -0300

    Laços de repetição

--- Variáveis e Operadores/exercicio2/exercicio2/obj/Debug/net8.0/exercicio2.AssemblyInfo.cs
+++ Variáveis e Operadores/exercicio2/exercicio2/obj/Debug/net8.0/exercicio2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("exercicio2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+002afaaaaa87dbc4d5c2ce7715fbdebb0309f0c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("exercicio2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("exercicio2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
