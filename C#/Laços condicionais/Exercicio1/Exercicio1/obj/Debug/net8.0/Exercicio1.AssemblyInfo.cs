09ba55e10a2bbbb54a72a113d89396c44859f382
Rebeka <rebeka.silva.emailprofissional@gmail.com>
2025-03-26 23:08:59 -0300

    Estilização e estrutura html

--- C#/Laços condicionais/Exercicio1/Exercicio1/obj/Debug/net8.0/Exercicio1.AssemblyInfo.cs
+++ C#/Laços condicionais/Exercicio1/Exercicio1/obj/Debug/net8.0/Exercicio1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercicio1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+555a5d51200fbaacb8d5472274d396e800103f67")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e01a97119b5ab2c673e96e818f000a8a9bfcf695")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercicio1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercicio1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
